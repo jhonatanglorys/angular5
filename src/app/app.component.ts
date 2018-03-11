25dfc74743f6f1382322d5738d077c971f72d1a3
jhonatanglorys <jhonatanglorys@gmail.com>
2018-03-10 23:20:55 -0500

    Json Lifecycle method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  query: String;
+  artists: Object;
+
+  showArtist(item){
+    this.query = item.name;
+    item.highlight = !item.highlight;
+  }
+  constructor(private http:HttpClient){
+    this.query = '';
+  }
+  ngOnInit():void{
+    this.http.get<Object>('../assets/datos.json').subscribe(
+      data => {
+        this.artists = data;
+      }
+    )
+  }
 }
